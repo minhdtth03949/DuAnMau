1c9cb85212fc51501a27983439d5bec2f010d64f
minhth03949 <minhdtth03949@fpt.edu.vn>
2025-06-07 09:13:18 +0700

    QLBanHang

--- DuAnMau/src/Controller/QLBanHang.java
+++ DuAnMau/src/Controller/QLBanHang.java
@@ -4,9 +4,12 @@
  */
 package Controller;
 
+import Model.ChiTietHoaDon;
+import Model.ChiTietSanPham;
 import Model.HoaDon;
 import java.sql.*;
 import Model.HoaDon;
+import Model.SanPham;
 import java.util.List;
 import java.sql.*;
 import java.util.ArrayList;
@@ -41,6 +44,67 @@ public class QLBanHang {
         }
         return lst;
     }
+    
+    public List<ChiTietHoaDon> getAllChiTietHoaDon() throws SQLException, ClassNotFoundException {
+        String query = "SELECT * FROM ChiTietHoaDon where MaHoaDon = ?";
+        Connection conn = mc.DBConnect();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        List<ChiTietHoaDon> lst = new ArrayList<>();
+        while (rs.next()) {
+            ChiTietHoaDon cthd = new ChiTietHoaDon();
+            cthd.setMaCTHD(rs.getInt(1));
+            cthd.setMaHD(rs.getInt(2));
+            cthd.setMaKM(rs.getInt(3));
+            cthd.setMaCTSP(rs.getInt(4));
+            cthd.setTenSP(rs.getString(5));
+            cthd.setDonGia(rs.getFloat(6));
+            cthd.setGiaApDungMaKM(rs.getFloat(7));
+            cthd.setTrangThai(rs.getBoolean(8));
+            lst.add(cthd);
+        }
+ 
+        return lst;
+    }
+    
+    public List<SanPham> getAllSanPham() throws SQLException, ClassNotFoundException {
+        List<SanPham> listSP = new ArrayList<>();
+        String query = "SELECT * FROM SanPham";
+        Connection connect = mc.DBConnect();
+        Statement stmt = connect.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            SanPham sp = new SanPham();
+            sp.setMaSP(rs.getInt(1));
+            sp.setTenSP(rs.getString(2));
+            sp.setDonGia(rs.getFloat(3));
+            sp.setNgayNhap(rs.getDate(4));
+            sp.setMaNV(rs.getInt(5));
+            listSP.add(sp);
+        }
+        return listSP;
+    }
+    
+    public List<ChiTietSanPham> getAllChiTietSanPham() throws SQLException, ClassNotFoundException {
+        String query = "SELECT * FROM ChiTietSanPham";
+        Connection conn = mc.DBConnect();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        List<ChiTietSanPham> lst = new ArrayList<>();
+        while (rs.next()) {
+            ChiTietSanPham ctsp = new ChiTietSanPham();
+            ctsp.setMaCTSP(rs.getInt(1));
+            ctsp.setMaSP(rs.getInt(2));
+            ctsp.setTenSP(rs.getString(3));
+            ctsp.setDonGia(rs.getFloat(4));
+            ctsp.setNgayNhap(rs.getDate(5));
+            ctsp.setHanSD(rs.getDate(6));
+            lst.add(ctsp);
+        }
+        
+        return lst;
+    }
+    
     
     }
     
--- DuAnMau/src/view/BanHang.java
+++ DuAnMau/src/view/BanHang.java
@@ -26,17 +26,75 @@ public class BanHang extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable3 = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
+
+        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane3.setViewportView(jTable3);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 818, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 605, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 605, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 605, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 213, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 494, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(41, 41, 41)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(43, 43, 43)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -78,5 +136,11 @@ public class BanHang extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JTable jTable3;
     // End of variables declaration//GEN-END:variables
 }
