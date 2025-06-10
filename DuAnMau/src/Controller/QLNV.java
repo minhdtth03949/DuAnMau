6ed6e392efe110448fac63376c3d93452cc492db
Karmal-17 <nghiepndtg00418@gmail.com>
2025-06-10 10:05:29 +0700

    Thêm Sửa Xoá

--- DuAnMau/src/Controller/QLNV.java
+++ DuAnMau/src/Controller/QLNV.java
@@ -82,7 +82,7 @@ public class QLNV {
             PreparedStatement pstm = con.prepareStatement(SQL);
             pstm.setInt(1, TheoMa);
             if (pstm.executeUpdate() > 0) {
-                System.out.println("Connect");
+                System.out.println("Xoa. Connect");
                 return 1;
             }
         } catch (Exception e) {
@@ -91,7 +91,49 @@ public class QLNV {
     }
 
     // Sửa Dữ Liệu Nhan Viên
-    public int SuaDL(NhanVien nv, String Theoma) {
-        String sql = "";
+    public int SuaDL(NhanVien nv, int Theoma) {
+        String sql = "UPDATE NhanVien SET MaNV = ?  ,\n"
+                + "                    TenDangNhap = ?  ,\n"
+                + "		       Email = ? ,\n"
+                + "		       MatKhau = ? ,\n"
+                + "		       VaiTro = ? \n"
+                + "		       WHERE MaNV = ?";
+        try {
+            Connection con = conn.DBConnect();
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setInt(1 , nv.getMaNV());
+            pstm.setString(2, nv.getTenDangNhap());
+            pstm.setString(3, nv.getEmail());
+            pstm.setString(4, nv.getMatKhau());
+            pstm.setBoolean(5, nv.getVaiTro());
+            pstm.setInt(6, Theoma);
+            if (pstm.executeUpdate() > 0) {
+                System.out.println("Sua. Connect"); 
+                return 1;
+           }
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+    // Tìm Kiếm Theo Mã Nhân Viên
+    public List<NhanVien> TimKiem(int TheoMa) throws SQLException, ClassNotFoundException {
+        List<NhanVien> listNV = new ArrayList<NhanVien>();
+        String query = "SELECT * FROM NHANVIEN";
+        Connection connect = conn.DBConnect();
+        Statement stmt = connect.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            NhanVien nv = new NhanVien();
+            nv.setMaNV(rs.getInt(1));
+            nv.setTenDangNhap(rs.getString(2));
+            nv.setEmail(rs.getString(3));
+            nv.setMatKhau(rs.getString(4));
+            nv.setVaiTro(rs.getBoolean(5));
+            if (nv.getMaNV() == TheoMa) {
+                System.out.println("Tim Kiem. Connect");
+                listNV.add(nv); 
+            }           
+        }
+        return listNV;
     }
 }
